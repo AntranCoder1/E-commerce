708a2236aadd0d68121eb5ce35e03481f3085ca8
thanhantran1 <thanhantran21@gmail.com>
2021-11-26 17:47:13 +0700

    Add function login on server side

--- server/routes/Auth.router.js
+++ server/routes/Auth.router.js
@@ -25,4 +25,44 @@ router.post("/register", async (req, res) => {
     }
 });
 
+// @router api/auth/login
+// @desc POST auth
+// @access Public
+router.post("/login", async (req, res) => {
+    try {
+        const user = await User.findOne({
+            username: req.body.username
+        });
+
+        !user && res.status(401).json({success: false, message: 'Wrong User Name' })
+
+        const hashedPassword = CryptoJS.AES.decrypt(
+            user.password,
+            process.env.PASS_SEC
+        );
+
+        const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+
+        const inputPassword = req.body.password;
+
+        originalPassword != inputPassword && 
+            res.status(401).json({ success: false, message: 'Wrong Password' });
+
+        const accessToken = jwt.sign(
+            {
+                id: user._id,
+                isAdmin: user.isAdmin
+            },
+            process.env.PASS_SEC, {
+                expiresIn: "3d"
+            }
+        );
+
+        const { password, ...orthers } = user._doc;
+        res.status(200).json({ ...orthers, accessToken });
+    } catch (error) {
+        res.status(500).json({ success: false, message: 'Internal server error' });
+    }
+});
+
 module.exports = router;
