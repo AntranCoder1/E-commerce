7526502e0090de43bea098d18f4d58dc49ea1601
thanhantran1 <thanhantran21@gmail.com>
2021-11-27 14:27:24 +0700

    Add function find user on server side

--- server/routes/User.router.js
+++ server/routes/User.router.js
@@ -4,12 +4,13 @@ const User = require('../models/User.models');
 const { 
     verifyToken, 
     verifyTokenAndAuthorization,
+    verifyTokenAndAdmin
 } = require('../verifyToken');
 
 // @router api/users/
 // @desc PUT users
 // @access Private
-router.put("/:id", verifyToken, async (req, res) => {
+router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
     if (req.body.password) {
         req.body.password = CryptoJS.AES.encrypt(
             req.body.password,
@@ -38,6 +39,19 @@ router.delete("/:id", verifyTokenAndAuthorization, async(req, res) => {
     try {
         await User.findByIdAndDelete(req.params.id);
         res.status(200).json('User has been deleted...');
+    } catch (error) {
+        res.status(500).json({ success: false, message: 'Internal server error' });
+    }
+});
+
+// @router api/users/find/:id
+// @desc GET users
+// @access Private
+router.get("/find/:id", verifyTokenAndAdmin, async (req, res) => {
+    try {
+        const getUser = await User.findById(req.params.id);
+        const { password, ...other } = getUser._doc;
+        res.status(200).json({ ...other });
     } catch (error) {
         res.status(500).json({ success: false, message: 'Internal server error' });
     }
