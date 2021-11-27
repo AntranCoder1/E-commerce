ba48ff2eb052436d41fb77f088b317ace06c3fd8
thanhantran1 <thanhantran21@gmail.com>
2021-11-27 14:07:15 +0700

    Add function deleted user on server side

--- server/routes/User.router.js
+++ server/routes/User.router.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User.models');
-const { verifyToken } = require('../verifyToken');
+const { 
+    verifyToken, 
+    verifyTokenAndAuthorization,
+} = require('../verifyToken');
 
 // @router api/users/
 // @desc PUT users
@@ -23,6 +26,18 @@ router.put("/:id", verifyToken, async (req, res) => {
             { new: true }
         );
         res.status(200).json(updatedUser);
+    } catch (error) {
+        res.status(500).json({ success: false, message: 'Internal server error' });
+    }
+});
+
+// @router api/users/:id
+// @desc DELETE users
+// @access Private
+router.delete("/:id", verifyTokenAndAuthorization, async(req, res) => {
+    try {
+        await User.findByIdAndDelete(req.params.id);
+        res.status(200).json('User has been deleted...');
     } catch (error) {
         res.status(500).json({ success: false, message: 'Internal server error' });
     }
