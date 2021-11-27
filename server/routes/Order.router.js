11f401b3406c3119b4cf088c5e3799a17110e206
thanhantran1 <thanhantran21@gmail.com>
2021-11-27 17:59:23 +0700

    Add function get income on server side

--- server/routes/Order.router.js
+++ server/routes/Order.router.js
@@ -74,4 +74,34 @@ router.get("/", verifyTokenAndAdmin, async (req, res) => {
     }
 });
 
+// @router api/orders/income
+// @desc GET income orders
+// @access Private
+router.get("/income", verifyTokenAndAdmin, async (req, res) => {
+    const date = new Date();
+    const lastMonth = new Date(date.setMonth(date.getMonth() - 1));
+    const previousMonth = new Date(new Date().setMonth(lastMonth.getMonth() - 1));
+
+    try {
+        const income = await Order.aggregate([
+            { $match: { createdAt: { $gte: previousMonth } } },
+            {
+                $project: {
+                    month: { $month: "$createdAt" },
+                    sales: "$amount",
+                },
+            },
+            {
+                $group: {
+                    _id: "$month",
+                    total: { $sum: "$sales" },
+                },
+            },
+        ]);
+        res.status(200).json(income);
+    } catch (error) {
+        res.status(500).json({ success: false, message: 'Internal server error' });
+    }
+});
+
 module.exports = router;
