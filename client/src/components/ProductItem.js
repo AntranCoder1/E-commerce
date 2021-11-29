30f900713f18222013b40486c17c78f0508f5858
thanhantran1 <thanhantran21@gmail.com>
2021-11-29 16:12:46 +0700

    Styled Components Hover & Animation on client

--- client/src/components/ProductItem.js
+++ client/src/components/ProductItem.js
@@ -13,12 +13,12 @@ const Info = styled.div`
     position: absolute;
     top: 0;
     left: 0;
-    background-color: rgba(0, 0, 0, .2);
+    background-color: rgba(0, 0, 0, 0.2);
     z-index: 3;
     display: flex;
     align-items: center;
     justify-content: center;
-    transform: all 0.5s ease;
+    transition: all 0.5s ease;
     cursor: pointer;
 `;
 
@@ -30,7 +30,7 @@ const Container = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
-    background-color: #5ffbfd;
+    background-color: #f5fbfd;
     position: relative;
 
     &:hover ${Info}{
@@ -42,7 +42,7 @@ const Circle = styled.div`
     width: 200px;
     height: 200px;
     border-radius: 50%;
-    background-color: #fff;
+    background-color: white;
     position: absolute;
 `;
 
@@ -55,13 +55,14 @@ const Icon = styled.div`
     width: 40px;
     height: 40px;
     border-radius: 50%;
-    background-color: #fff;
+    background-color: white;
     display: flex;
     align-items: center;
     justify-content: center;
     margin: 10px;
     transition: all 0.5s ease;
-    &::hover {
+    
+    &:hover {
         background-color: #e9f5f5;
         transform: scale(1.1);
     }
