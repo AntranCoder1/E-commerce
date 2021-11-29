c3085e5092dc7658794ee7fa2208f341683caf69
thanhantran1 <thanhantran21@gmail.com>
2021-11-29 17:43:52 +0700

    Add login and register design on client side

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import Home from './pages/Home';
 import ProductList from './pages/ProductList';
 import Product from './pages/Product';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   return (
-    <Product />
+    <Register />
   );
 }
 
