6cb14d04b92e2d7174936ee30f7a477aa104644d
thanhantran1 <thanhantran21@gmail.com>
2021-11-29 16:43:36 +0700

    Add Footer Design on client side

---
+++ client/src/components/Footer.js
@@ -0,0 +1,140 @@
+import React from 'react';
+import styled from 'styled-components';
+import {
+    Facebook,
+    Instagram,
+    MailOutline,
+    Phone,
+    Pinterest,
+    Room,
+    Twitter,
+} from "@material-ui/icons";
+
+const Container = styled.div`
+    display: flex;
+`;
+
+const Left = styled.div`
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    padding: 20px;
+`;
+
+const Logo = styled.h1``;
+
+const Desc = styled.p`
+    margin: 20px 0;
+`;
+
+const SocialContainer = styled.div`
+    display: flex;
+`;
+
+const SocialIcon = styled.div`
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    color: #fff;
+    background-color: #${props => props.color};
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-right: 20px;
+`;
+
+const Center = styled.div`
+    flex: 1;
+    padding: 20px;
+`;
+
+const Title = styled.h3`
+    margin-bottom: 30px;
+`;
+
+const List = styled.ul`
+    margin: 0;
+    padding: 0;
+    list-style: none;
+    display: flex;
+    flex-wrap: wrap;
+`;
+
+const ListItem = styled.li`
+    width: 50%;
+    margin-bottom: 10px;
+`;
+
+const Right = styled.div`
+    flex: 1;
+    padding: 20px;
+`;
+
+const ContactItem = styled.div`
+    margin-bottom: 20px;
+    display: flex;
+    align-items: center;
+`;
+
+const Payment = styled.img`
+    width: 50%;
+`;
+
+const Footer = () => {
+    return (
+        <Container>
+            <Left>
+                <Logo>ANTRAN.</Logo>
+                <Desc>
+                    There are many variations of passages of Lorem Ipsum available, but
+                    the majority have suffered alteration in some form, by injected
+                    humour, or randomised words which don’t look even slightly believable.
+                </Desc>
+                <SocialContainer>
+                    <SocialIcon color="3b5999">
+                        <Facebook />
+                    </SocialIcon>
+                    <SocialIcon color="e4405f">
+                        <Instagram />
+                    </SocialIcon>
+                    <SocialIcon color="55acee">
+                        <Twitter />
+                    </SocialIcon>
+                    <SocialIcon color="e60023">
+                        <Pinterest />
+                    </SocialIcon>
+                </SocialContainer>
+            </Left>
+            <Center>
+                <Title>Useful Links</Title>
+                <List>
+                    <ListItem>Home</ListItem>
+                    <ListItem>Cart</ListItem>
+                    <ListItem>Man Fashion</ListItem>
+                    <ListItem>Woman Fashion</ListItem>
+                    <ListItem>Accessories</ListItem>
+                    <ListItem>My Account</ListItem>
+                    <ListItem>Order Tracking</ListItem>
+                    <ListItem>Wishlist</ListItem>
+                    <ListItem>Wishlist</ListItem>
+                    <ListItem>Terms</ListItem>
+                </List>
+            </Center>
+            <Right>
+                <Title>Contact</Title>
+                <ContactItem>
+                    <Room style={{marginRight:"10px"}}/> 35/10/50 Phan Van Hon, q.12, TP.HCM, VN
+                </ContactItem>
+                <ContactItem>
+                    <Phone style={{marginRight:"10px"}}/> +84 98 210 6287
+                </ContactItem>
+                <ContactItem>
+                    <MailOutline style={{marginRight:"10px"}} /> contact@antran.com
+                </ContactItem>
+                <Payment src="https://i.ibb.co/Qfvn4z6/payment.png" />
+            </Right>
+        </Container>
+    )
+}
+
+export default Footer
