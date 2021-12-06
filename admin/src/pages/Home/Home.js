694e63d8fff1ebf2446b03448a7de7bd7d445ea3
thanhantran1 <thanhantran21@gmail.com>
2021-12-06 18:19:29 +0700

    React.js Chart on admin side

--- admin/src/pages/Home/Home.js
+++ admin/src/pages/Home/Home.js
@@ -1,16 +1,55 @@
-import React from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import Chart from '../chart/Chart';
 import FeaturedInfo from '../featuredInfo/FeaturedInfo';
 import { data } from '../../data';
 import './Home.css';
 import WidgetSm from '../widgetSm/WidgetSm';
 import Widgetlg from '../Widgetlg/Widgetlg';
+import { userRequest } from '../../requestMethod';
 
 const Home = () => {
+
+    const [userStats, setUserStats] = useState([]);
+
+    const MONTHS = useMemo(
+        () => [
+            "Jan",
+            "Feb",
+            "Mar",
+            "Apr",
+            "May",
+            "Jun",
+            "Jul",
+            "Agu",
+            "Sep",
+            "Oct",
+            "Nov",
+            "Dec",
+        ],
+        []
+    );
+
+    useEffect(() => {
+        const getStats = async () => {
+            try {
+                const res = await userRequest.get("/users/stats");
+                res.data.map((item) =>
+                    setUserStats((prev) => [
+                        ...prev,
+                        { name: MONTHS[item._id - 1], "Active User": item.total },
+                    ])
+                );
+            } catch {}
+        };
+        getStats();
+    }, [MONTHS]);
+
+    console.log(userStats)
+
     return (
         <div className="home">
             <FeaturedInfo />
-            <Chart data={data} title="User Analytics" grid datakey="uv" />
+            <Chart data={userStats} title="User Analytics" grid datakey="Active User" />
             <div className="homwWidgets">
                 <WidgetSm />
                 <Widgetlg />
